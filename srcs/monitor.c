8563aafb82f122faa6c8480f8543fa10feb8c0f6
eel-abed <eli_elabed@hotmail.fr>
2025-01-28 17:58:10 +0100

    ajout du delai pour repas + repax eaten fin

--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,39 +6,49 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:58:10 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/01/27 20:56:12 by eel-abed         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:16:41 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void	*monitor(void *arg)
+void *monitor(void *arg)
 {
-	t_philo	*philos;
-	int		i;
-	int		all_full;
+    t_philo *philos;
+    int     i;
+    int     all_full;
 
-	philos = (t_philo *)arg;
-	while (1)
-	{
-		i = -1;
-		all_full = 1;
-		while (++i < philos->data->num_philos)
-		{
-			pthread_mutex_lock(&philos->data->meal_lock);
-			if (get_time() - philos[i].last_meal > philos->data->time_to_die)
-			{
-				print_status(&philos[i], "died");
-				exit(0);
-			}
-			if (philos->data->max_meals > 0 && 
-				philos[i].meals_eaten < philos->data->max_meals)
-				all_full = 0;
-			pthread_mutex_unlock(&philos->data->meal_lock);
-		}
-		if (all_full && philos->data->max_meals > 0)
-			exit(0);
-		usleep(1000);
-	}
-	return (NULL);
+    philos = (t_philo *)arg;
+    while (1)
+    {
+        i = -1;
+        all_full = 1;
+        while (++i < philos->data->num_philos)
+        {
+            pthread_mutex_lock(&philos->data->meal_lock);
+            if (get_time() - philos[i].last_meal > philos->data->time_to_die)
+            {
+                print_status(&philos[i], "died");
+                // Print meals eaten before exit
+                for (int j = 0; j < philos->data->num_philos; j++)
+                    printf("Philosopher %d ate %d meals\n", philos[j].id, philos[j].meals_eaten);
+                exit(0);
+            }
+            if (philos->data->max_meals > 0 && 
+                philos[i].meals_eaten < philos->data->max_meals)
+                all_full = 0;
+            pthread_mutex_unlock(&philos->data->meal_lock);
+        }
+        if (all_full && philos->data->max_meals > 0)
+        {
+            // Print meals eaten when all philosophers are full
+            pthread_mutex_lock(&philos->data->print_lock);
+            for (int j = 0; j < philos->data->num_philos; j++)
+                printf("Philosopher %d ate %d meals\n", philos[j].id, philos[j].meals_eaten);
+            pthread_mutex_unlock(&philos->data->print_lock);
+            exit(0);
+        }
+        usleep(1000);
+    }
+    return (NULL);
 }
--- srcs/philosopher.c
+++ srcs/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: eel-abed <eel-abed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:57:49 by eel-abed          #+#    #+#             */
-/*   Updated: 2025/01/27 20:29:11 by eel-abed         ###   ########.fr       */
+/*   Updated: 2025/01/28 17:18:04 by eel-abed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,25 @@ void	*philo_routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	// Add initial delay for even-numbered philosophers
+	if (philo->id % 2 == 0)
+		ft_usleep(philo->data->time_to_eat / 2);
+
 	while (1)
 	{
 		take_forks(philo);
-
 		eat(philo);
-
 		release_forks(philo);
+		
+		// Check if max meals reached before continuing
+		pthread_mutex_lock(&philo->data->meal_lock);
+		if (philo->data->max_meals > 0 && 
+			philo->meals_eaten >= philo->data->max_meals)
+		{
+			pthread_mutex_unlock(&philo->data->meal_lock);
+			break;
+		}
+		pthread_mutex_unlock(&philo->data->meal_lock);
 
 		print_status(philo, "is sleeping");
 		ft_usleep(philo->data->time_to_sleep);
